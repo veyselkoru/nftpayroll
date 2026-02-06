e9058af90653ca8130cf2bcf6a32cca6a3248987
Veysel <veysel@Veysel-MacBook-Pro.local>
2026-02-06 17:58:40 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    base: process.env.ASSET_URL || '/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
